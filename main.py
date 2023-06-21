616b8ce714420d2ef5a969b18b671374e0a81e01
Abolfazl <132772262+AbolfazlYavari@users.noreply.github.com>
2023-06-21 09:57:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Importing required libraries
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QMenuBar, QStatusBar, QWidget, QFrame, \
     QCheckBox, QSlider, QTextEdit, QMessageBox
 from PyQt5.QtGui import QClipboard
@@ -10,10 +11,11 @@ from random import randint
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
-
+        
+        # Load the UI file
         uic.loadUi("password-generator.ui", self)
 
-        
+        # Find UI elements
         self.textedit = self.findChild(QTextEdit, "textEdit_2")
         self.generatepass = self.findChild(QPushButton, "pushButton")
         self.clipboard = self.findChild(QPushButton, "pushButton_3")
@@ -28,26 +30,40 @@ class UI(QMainWindow):
         self.Symbols = self.findChild(QCheckBox, "checkBox_4")
         self.slider = self.findChild(QSlider, "horizontalSlider")
 
+        # Set default checkbox values
         self.Uppercase.setChecked(True)
         self.Lowercase.setChecked(True)
         self.Numbers.setChecked(True)
         self.Symbols.setChecked(True)
 
+        # Connect button clicks to functions
         self.generatepass.clicked.connect(self.passgenerate)
         self.clipboard.clicked.connect(self.copy)
         self.slider.valueChanged.connect(self.slide_it)
         self.slider.setMinimum(1)
 
-
+        # Show the UI
         self.show()
 
     def slide_it(self, value):
+         """
+        Updates the label text when the slider value changes.
+
+        Args:
+            value (int): The new value of the slider.
+        """
 
         global plength
         plength = value
         self.label2.setText(str(value))
 
     def passgenerate(self):
+        """
+        Generates a password based on the selected checkboxes and displays it in the textedit.
+
+        Raises:
+            QMessageBox.warning: If no checkbox is selected.
+        """
         global plength
         
 
@@ -210,6 +226,9 @@ class UI(QMainWindow):
             self.textedit.setText(my_password)
 
     def copy(self):
+        """
+        Copies the password from the textedit to the clipboard.
+        """
         text = self.textedit.toPlainText()
         if text:
             app = QApplication.instance()
